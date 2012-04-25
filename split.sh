eb33d3980fa45d64698c614a5240f42c1e2d7c88
Jesse Zhang <jessezhang@vmware.com>
2012-04-24 21:47:47 -0700

    Initial commit
      Usage: bash split.sh ~/src/vcap /tmp

---
+++ split.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -uxe
+
+if [ $# -ne 2 ]; then
+	echo "expected two arguments given $#"
+	exit 1
+fi
+
+VCAP_REPO=$1
+CLONE_BASE=$2
+CLONE_PATH=$(mktemp -d "$2/vcap.XXX")
+
+git clone $VCAP_REPO $CLONE_PATH
+cd $CLONE_PATH
+for c in common cloud_controller dea health_manager router staging stager; do
+	git checkout --quiet origin/master
+	git checkout -b small_$c
+	git filter-branch -f --prune-empty --subdirectory-filter $c --index-filter "git rm --quiet --cached --ignore-unmatch -r -f vendor/cache"
+	git push -n -v git@github.com:d/vcap-$c.git HEAD:master
+done
+git branch -v
