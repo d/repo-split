d55653b930c9ac8b7aed2c931d469839f00a2839
Jesse Zhang <jessezhang@vmware.com>
2012-04-24 22:04:55 -0700

    Push up to github

--- split.sh
+++ split.sh
@@ -17,6 +17,6 @@ for c in common cloud_controller dea health_manager router staging stager; do
 	git checkout --quiet origin/master
 	git checkout -b small_$c
 	git filter-branch -f --prune-empty --subdirectory-filter $c --index-filter "git rm --quiet --cached --ignore-unmatch -r -f vendor/cache"
-	git push -n -v git@github.com:d/vcap-$c.git HEAD:master
+	git push -v git@github.com:d/vcap-$c.git HEAD:master
 done
 git branch -v
