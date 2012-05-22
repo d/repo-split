95f1ffd3cf8eee93c762f92f3866f1fec310d8d2
Jesse Zhang <jessezhang@vmware.com>
2012-05-22 16:10:54 -0700

    split cc and hm; split package_cache and friends

--- split.sh
+++ split.sh
@@ -20,3 +20,23 @@ for c in router staging stager; do
 	git filter-branch -f --prune-empty --subdirectory-filter $c --index-filter "git rm --quiet --cached --ignore-unmatch -r -f vendor/cache"
 	git push -v git@github.com:d/vcap-$c.git HEAD:master
 done
+
+# package_cache and friends
+git checkout --quiet origin/master
+git checkout -b package_cache-and-friends
+INDEX_FILTER="git rm -r -f --cached --quiet --ignore-unmatch \$(git ls-tree --name-only \$GIT_COMMIT | egrep -v '^package_cache|package_cache_client|.gitignore' ) \*/vendor/cache/\*"
+git filter-branch -f --prune-empty \
+	--index-filter "$INDEX_FILTER" \
+	--parent-filter $SCRIPT_DIR/parent-filter.rb
+git push -v git@github.com:d/package_cache-and-friends.git HEAD:master
+
+git checkout --quiet origin/master
+git checkout -b cchm
+INDEX_FILTER="git rm -r -f --cached --quiet --ignore-unmatch \$(git ls-tree --name-only \$GIT_COMMIT | egrep -v '^cloud_controller|health_manager|.gitignore' ) \*/vendor/cache/\*"
+git filter-branch -f --prune-empty \
+	--index-filter "$INDEX_FILTER" \
+	--parent-filter $SCRIPT_DIR/parent-filter.rb
+git push -v git@github.com:d/cchm.git HEAD:master
+
+
+git branch -v
